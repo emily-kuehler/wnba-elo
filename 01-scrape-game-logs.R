d3312efe5d75ea8ba7892e3df5fcb18e3919cd4f
Emily Kuehler <ekuehler@gmail.com>
2021-05-27 15:58:17 -0700

    close connections, function to scrape all historical gamelogs

--- 01-scrape-game-logs.R
+++ 01-scrape-game-logs.R
@@ -8,6 +8,9 @@ library(lubridate)
 get_wnba_bball_ref_team_urls <- function() {
   
   base_teams_url <- "https://www.basketball-reference.com/wnba/teams/"
+  base_teams_url <- try_catch_get_url(base_teams_url)
+  
+  #base_teams_url <- url(base_teams_url, "rb")
   
   webpage <- read_html(base_teams_url)
   
@@ -57,6 +60,8 @@ get_wnba_bball_ref_team_urls <- function() {
   team_df_unique_url <- team_df %>% 
     anti_join(duplicate_urls)
   
+  #close.connection(base_teams_url)
+  
   return (team_df_unique_url)
   
 }
@@ -64,14 +69,19 @@ get_wnba_bball_ref_team_urls <- function() {
 
 # scrape team game logs ---------------------------------------------------
 
-team_urls <- get_wnba_bball_ref_team_urls()
-
-#function will take a url and a season, then scrape game info
-
-
-try_catch_read_html <- function(x) {
+#actually reads the html
+try_catch_read_html <- function(url_con) {
   tryCatch({
-    read_html(x)
+    read_html(url_con)
+  }, error = function(e) {
+    NA
+  })
+}
+
+#just returns a url object
+try_catch_get_url <- function(x) {
+  tryCatch({
+    url_obj <- url(x)
   }, error = function(e) {
     NA
   })
@@ -80,13 +90,15 @@ try_catch_read_html <- function(x) {
 
 get_season_game_logs <- function(team_url, season, team_url_df) {
   
-  team_game_log_url <- paste0("https://www.basketball-reference.com/", team_url, season, "_games.html")
+  team_game_log_url_str <- paste0("https://www.basketball-reference.com/", team_url, season, "_games.html")
   
-  webpage <- try_catch_read_html(team_game_log_url)
+  team_game_log_url <- try_catch_get_url(team_game_log_url_str)
   
   curr_team <- team_url_df %>% 
     filter(url == team_url) %>% 
     pull(Team)
+  
+  webpage <- try_catch_read_html(team_game_log_url)
   
   if (!is.na(webpage)) {
     
@@ -142,13 +154,37 @@ get_season_game_logs <- function(team_url, season, team_url_df) {
       bind_rows(game_logs_post)
     
     game_logs <- game_logs %>% 
-      mutate(team = curr_team) %>% 
-      select(-game_id) %>% 
-      mutate(game_num = row_number())
+      mutate(team = curr_team,
+             team_game_num = row_number(),
+             home_team = ifelse(home_away == '',team,opponent),
+             visit_team = ifelse(home_away == '@',team,opponent),
+             season = season,
+             club_code = team_url,
+             win_loss = ifelse(win_loss == "W",1,0)) %>% 
+      select(team_game_num,
+             date,
+             team,
+             opponent,
+             home_team,
+             visit_team,
+             team_pts,
+             opp_pts,
+             win_loss,
+             season_type,
+             season,
+             club_code
+             )
+    
+    #close.connection(team_game_log_url)
+    
+    print(paste0("got ", season, " data for ", curr_team))
     
     return (game_logs)
     
   } else {
+    
+    close.connection(team_game_log_url)
+    print(showConnections())
     
     return (NULL)
     
@@ -156,9 +192,30 @@ get_season_game_logs <- function(team_url, season, team_url_df) {
   
 }
 
-game_logs_df <- get_season_game_logs(team_urls$url[1], 2014, team_urls)
 
+# scrape all historical gamelogs ------------------------------------------
 
+scrape_historical_gamelogs <- function() {
+  
+  team_url_df <- get_wnba_bball_ref_team_urls()
+  
+  first_wnba_season <- min(team_url_df$From)
+  last_completed_wnba_season <- max(team_url_df$To) - 1
+  #last_completed_wnba_season <- 2002
+  seasons <- seq(from = first_wnba_season, to = last_completed_wnba_season)
+  
+  unique_team_urls <- unique(team_url_df$url)
+  
+  url_season_list <- list(url = unique_team_urls, season = seasons)
+  url_season_cross <- cross_df(url_season_list)
+  
+  historical_game_logs <- map2_df(.x = url_season_cross$url, .y = url_season_cross$season, .f = get_season_game_logs, team_url_df)
+  
+  return (historical_game_logs)
+  
+}
+
+historical_game_logs <- scrape_historical_gamelogs()
 
 
 
